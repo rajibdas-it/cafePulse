cb8cf62c6499e7057bb185eccaee57b7683795d3
rajibdas-it <rajibrad@gmail.com>
2023-12-27 00:38:49 +0600

    about section done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,12 +6,15 @@ const About = () => {
       <h1 className="font-semibold text-center text-4xl lg:mt-14 mt-24 mb-8">
         About Us
       </h1>
-      <div>
-        <div>
+
+      <div className="flex flex-col lg:flex-row items-center gap-5 ">
+        <div className="w-full lg:w-2/4 ">
           <img src={img} alt="img" />
         </div>
-        <div>
-          <h2>What Makes Our Coffee Special?</h2>
+        <div className="w-full lg:w-2/4 p-4 space-y-3 ">
+          <h2 className="font-semibold text-3xl">
+            What Makes Our Coffee Special?
+          </h2>
           <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci
             ex dicta architecto, labore porro facilis enim ullam odio
