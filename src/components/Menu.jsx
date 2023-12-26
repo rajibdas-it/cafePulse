ae2f5e7c6f97dcfbd1eb1afb609eb5ef14fff48f
rajibdas-it <rajibrad@gmail.com>
2023-12-26 21:49:26 +0600

    our menu section done

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,10 @@
 import MenuCard from "../layouts/MenuCard";
 import img1 from "../assets/img/menu1.jpg";
+import img2 from "../assets/img/menu2.jpg";
+import img3 from "../assets/img/menu3.jpg";
+import img4 from "../assets/img/menu4.jpg";
+import img5 from "../assets/img/menu5.jpg";
+import img6 from "../assets/img/menu6.jpg";
 
 const Menu = () => {
   return (
@@ -7,8 +12,13 @@ const Menu = () => {
       <h1 className="font-semibold text-center text-4xl mt-24 mb-8">
         Our Menu
       </h1>
-      <div>
-        <MenuCard img={img1} title="Expresso" value="500" />
+      <div className="flex flex-wrap pb-8 gap-8 justify-center">
+        <MenuCard img={img1} title="Expresso" value="5" />
+        <MenuCard img={img2} title="Cappuccino" value="10" />
+        <MenuCard img={img3} title="Latte" value="10" />
+        <MenuCard img={img4} title="Americano" value="12" />
+        <MenuCard img={img5} title="Macchiato" value="15" />
+        <MenuCard img={img6} title="Doppio" value="10" />
       </div>
     </div>
   );
--- src/layouts/MenuCard.jsx
+++ src/layouts/MenuCard.jsx
@@ -2,21 +2,25 @@
 import { FaShoppingCart } from "react-icons/fa";
 const MenuCard = (props) => {
   return (
-    <div className="w-full lg:w-1/4 bg-white p-2 rounded-lg">
+    <div className="w-full lg:w-1/4 bg-white p-3 rounded-lg">
       <div>
-        <img src={props?.img} alt="img" className="rounded-lg" />
+        <img src={props?.img} alt="img" className="rounded-xl" />
       </div>
-      <div>
-        <div>
-          <h3>{props?.title}</h3>
-          <h3>{props?.value}</h3>
+      <div className="p-2 mt-5">
+        <div className="flex flex-row justify-between">
+          <h3 className="font-semibold text-xl">{props?.title}</h3>
+          <h3 className="font-semibold text-xl">{props?.value}</h3>
         </div>
-        <div>
-          <div>
-            <button>Hot</button>
-            <button>Cold</button>
+        <div className="flex flex-row justify-between mt-3">
+          <div className="flex gap-2">
+            <button className="px-3 text-sm border-2 border-brightColor bg-backgroundColor hover:text-brightColor transition-all rounded-lg">
+              Hot
+            </button>
+            <button className="px-3 text-sm border-2 border-brightColor bg-backgroundColor hover:text-brightColor transition-all rounded-lg">
+              Cold
+            </button>
           </div>
-          <span>
+          <span className="flex items-center bg-backgroundColor px-3 py-2 rounded-full cursor-pointer">
             <FaShoppingCart size={20} />
           </span>
         </div>
