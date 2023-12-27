e74a74cd7bec9056eb82c23dc4abcb24ff87f673
rajibdas-it <rajibrad@gmail.com>
2023-12-27 12:35:08 +0600

    product section done

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,19 @@
 import ProductCard from "../layouts/ProductCard";
 
-import img from "../assets/img/product1.jpg";
-import img1 from "../assets/img/product2.jpg";
-import img2 from "../assets/img/product3.jpg";
+import img1 from "../assets/img/product1.jpg";
+import img2 from "../assets/img/product2.jpg";
+import img3 from "../assets/img/product3.jpg";
 
 const Product = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-brightColor px-5 lg:px-32">
-      <h1 className="font-semibold text-center text-4xl lg:mt-14 mt-14 mt-024 mb-8">
+    <div className="min-h-screen flex flex-col justify-center bg-backgroundColor px-5 lg:px-32">
+      <h1 className="font-semibold text-center text-4xl lg:mt-14 mt-24 mb-8">
         Our Products
       </h1>
-      <div>
-        <ProductCard img={img} title="Nespresso" />
+      <div className="flex flex-col lg:flex-row justify-center gap-12 ">
         <ProductCard img={img1} title="Nespresso" />
-        <ProductCard img={img2} title="Nespresso" />
+        <ProductCard img={img2} title="AeroPress" />
+        <ProductCard img={img3} title="Chemex" />
       </div>
     </div>
   );
--- src/layouts/ProductCard.jsx
+++ src/layouts/ProductCard.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
 
-import { BsStarFill } from "react-icons/bs";
+import { BsStarFill, BsStarHalf } from "react-icons/bs";
 import Button from "./Button";
 
 const ProductCard = (props) => {
   return (
-    <div className="w-full lg:1/4 bg-white ">
+    <div className="w-full lg:w-1/4 bg-white p-3 rounded-lg ">
       <img className="rounded-lg" src={props.img} alt="" />
       <div className="flex flex-col items-center mt-5 gap-3">
         <h2 className="font-semibold text-xl">{props.title}</h2>
@@ -14,9 +14,9 @@ const ProductCard = (props) => {
           <BsStarFill size={25} className="text-brightColor" />
           <BsStarFill size={25} className="text-brightColor" />
           <BsStarFill size={25} className="text-brightColor" />
-          <BsStarFill size={25} className="text-brightColor" />
+          <BsStarHalf size={25} className="text-brightColor" />
         </div>
-        <h3>$15.99</h3>
+        <h3 className="font-semibold text-xl">$15.99</h3>
         <Button title="ADD TO CART" />
       </div>
     </div>
